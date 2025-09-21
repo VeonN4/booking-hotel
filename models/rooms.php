61fa0a9588a4a99a1e79065b53f5e0d4a143656c
VeonN4 <fxzia13@gmail.com>
2025-09-21 12:54:35 +0700

    feat: add Room model

---
+++ models/rooms.php
@@ -0,0 +1,54 @@
+<?php
+include "./services/db.php";
+
+function createRoom($hotel_id, $room_number, $type, $price, $capacity, $status)
+{
+    $db = getDBConnection();
+    $query = "INSERT INTO rooms(hotel_id, room_number, type, price, capacity, status) VALUES (?, ?, ?, ?, ?, ?)";
+
+    $stmt = $db->prepare($query);
+    $stmt->bind_param("ssssss", $hotel_id, $room_number, $type, $price, $capacity, $status);
+    return $stmt->execute();
+}
+
+function getAllRooms()
+{
+    $db = getDBConnection();
+    $query = "SELECT * FROM rooms";
+
+    $stmt = $db->prepare($query);
+    $stmt->execute();
+    return $stmt->get_result();
+}
+
+function getRoomById($room_id)
+{
+    $db = getDBConnection();
+    $query = "SELECT * FROM rooms WHERE room_id = ?";
+
+    $stmt = $db->prepare($query);
+    $stmt->bind_param("s", $room_id);
+    $stmt->execute();
+    return $stmt->get_result();
+}
+
+function updateRoomById($room_id, $hotel_id, $room_number, $type, $price, $capacity, $status)
+{
+    $db = getDBConnection();
+    $query = "UPDATE rooms SET hotel_id = ?, room_number = ?, type = ?, price = ?, capacity = ?, status = ? WHERE room_id = ?";
+
+    $stmt = $db->prepare($query);
+    $stmt->bind_param("sssssss", $hotel_id, $room_number, $type, $price, $capacity, $status, $room_id);
+    return $stmt->execute();
+}
+
+function deleteRoomById($room_id)
+{
+    $db = getDBConnection();
+    $query = "DELETE FROM rooms WHERE room_id = ?";
+
+    $stmt = $db->prepare($query);
+    $stmt->bind_param("s", $room_id);
+    $stmt->execute();
+    return $stmt->get_result();
+}
