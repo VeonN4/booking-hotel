b23310f7fd70e0a3b14b82147702e9899cdd0276
VeonN4 <fxzia13@gmail.com>
2025-10-02 16:23:40 +0700

    feat: redesigned homepage

---
+++ views/layouts/nav.php
@@ -0,0 +1,11 @@
+<!-- Navigation -->
+<nav>
+    <div class="nav-container">
+        <div class="logo">Serene</div>
+        <ul class="nav-links">
+            <li><a href="#about">About</a></li>
+            <li><a href="#destinations">Destinations</a></li>
+            <li><a href="#contact">Contact</a></li>
+        </ul>
+    </div>
+</nav>
